41c890debf46bdee2e3b01db3860f22a5977cf15
Santhosh Marriboina <marriboinasanthosh7@gmail.com>
2022-03-15 19:32:17 +0530

    second commit

--- src/PrintNumber.java
+++ src/PrintNumber.java
@@ -4,6 +4,7 @@ public class PrintNumber {
 	public static void main(String[] args) {
 		int a = 10 ,b = 20;
 		System.out.println("sum of two numbers:" +(a+b));
+		System.out.println("multiplication of two number:" + (a*b));
 
 	}
 
